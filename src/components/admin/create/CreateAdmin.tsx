fc6f88cef95c2876833bba3934d0367b33c9b43c
shimjaedoo <woen1637@naver.com>
2024-11-10 16:06:49 +0900

    [Feat] : AdminCreate Component Form Add

---
+++ src/components/admin/create/CreateAdmin.tsx
@@ -0,0 +1,13 @@
+export default function CreateAdmin() {
+  return (
+    /** Body Tag Add --> */
+
+    /** BackGround Div */
+    <div className={`flex bg-gray-100 w-full h-screen justify-center pb-12 pt-12`}>
+      {/** Form Div */}
+      <div className={`bg-white w-[50rem] flex flex-col rounded-md shadow-md pt-12`}>
+        <div className={` text-[2.5rem] flex font-semibold text-gray-750 justify-center`}>Sign Up</div>
+      </div>
+    </div>
+  );
+}
