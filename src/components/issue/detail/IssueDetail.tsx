bf6e6658cd3bc9f0f64221555eba6194b0a4adf0
shimjaedoo <woen1637@naver.com>
2024-11-10 16:04:45 +0900

    [Fix] : Editor 라이브러리 적용 및 작성버튼 추가

--- src/components/issue/detail/IssueDetail.tsx
+++ src/components/issue/detail/IssueDetail.tsx
@@ -1,8 +1,15 @@
 import Body from "../../../_common/Body";
 import "@mdxeditor/editor/style.css";
 import Editor from "../../../_common/Editor";
+import React, { useState } from "react";
+import MDEditor from "@uiw/react-md-editor";
 
 export default function IssueDetail() {
+  /* UseState */
+  const [value, setValue] = useState<any>("**Hello world!!!**");
+  // function changeHandler(e: React.ChangeEvent<HTMLTextAreaElement>) {
+  //   setValue(e.target.value);
+  // }
   return (
     <Body title={"제목란"} description={`{작성자}가 이슈를 등록했습니다 {0}일 전 · {0}개의 댓글`}>
       {/** BackGround Area Div */}
@@ -14,21 +21,24 @@ export default function IssueDetail() {
           {/** 댓글 배경 Div */}
           <div className={` text-sm px-2 py-2 flex flex-col gap-8`}>
             {/** 댓글 아이템들 Div */}
-            <div className={` h-auto flex gap-5 `}>
+            <div className={` h-auto flex gap-5`}>
               {/** 댓글 프로필이미지 Div */}
               <div className={`flex-none flex-col items-center justify-center pl-1`}>
-                <img className={` w-[2.5rem] aspect-square rounded-full`} src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpSyoumS-w9MICsbUAy3VOMDsecUcgNaqAGA&s"} />
+                <img
+                  className={` w-[2.5rem] aspect-square rounded-full cursor-pointer`}
+                  src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpSyoumS-w9MICsbUAy3VOMDsecUcgNaqAGA&s"}
+                />
               </div>
               {/**  댓글 이름&&등록일&& 내용 부모 Div*/}
-              <div className={`flex flex-col gap-1 flex-grow `}>
+              <div className={`flex flex-col gap-1 flex-grow  `}>
                 {/** 댓글 이름 && 등록일 Div*/}
                 <div className={`flex gap-5 `}>
                   <p className={`text-[#6b7076] font-semibold`}>Jone Wick</p>
                   <p className={`text-[#6b7076] font-semibold`}>3 days ago</p>
                 </div>
                 {/** 댓글 내용 Div */}
-                <div className={` w-[100vh] break-words text-gray-800 `}>
-                  3asdasdasdasd3asdasdasdasd3asdasdasdasd3ascssdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3sdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdasasdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasda3asdasdasdasd3asdasdasdasdsdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd
+                <div className={`w-[100vh] break-words text-gray-800`}>
+                  dasd3asdasdasasdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3sdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdasasdasdasdasd3asdasdasdasd3asdasdassdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasda3asdasdasdasd3asdasdasdasdsdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd3asdasdasdasd
                 </div>
               </div>
             </div>
@@ -52,8 +62,19 @@ export default function IssueDetail() {
                 </div>
               </div>
             </div>
-            {/* MDX Editor */}
-            <Editor />
+            {/** Editor & Button Div */}
+            <div className={`flex flex-col gap-3`}>
+              {/* MDX Editor */}
+              {/*<Editor />*/}
+              <div className="w-full" data-color-mode={"light"}>
+                <MDEditor value={value} onChange={setValue} preview={"edit"} />
+                {/*<MDEditor.Markdown source={value} style={{ whiteSpace: "pre-wrap" }} />*/}
+              </div>
+              {/** Button Div */}
+              <div className={`flex justify-end items-center`}>
+                <button className={`bg-gray-50 w-[5rem] h-[2rem] border rounded-md flex items-center justify-center hover:bg-gray-200 duration-500 `}>작성</button>
+              </div>
+            </div>
           </div>
         </div>
         {/** 상태창 Div */}
